9932b7c12b8bfed312a07ef5f86b5885bda298bc
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-08-25 15:04:10 +0200

    feat: Add route for logout

--- auth-service/src/routes/authRoutes.ts
+++ auth-service/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { registerController, loginController, refreshController } from "../controllers/authController";
+import { registerController, loginController, refreshController, logoutController } from "../controllers/authController";
 
 export default async function authRoutes(app: FastifyInstance) {
     app.post("/login", loginController);
     app.post("/register", registerController);
     app.post("/refresh", refreshController);
+    app.post("/logout", logoutController);
 }
