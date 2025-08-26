2a531c74a83718197ded3db31d003bcccf6840e3
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-08-26 17:56:40 +0200

    feat: Add refreshToken cookies and service logic
    Now the refreshToken is save and read from the cookies, improving
    security.
    
    Also, I have extract some of the business logic for refreshController
    and logoutController into services, for consistence.

--- auth-service/src/controllers/authController.ts
+++ auth-service/src/controllers/authController.ts
@@ -1,9 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { registerUser, loginUser } from "../services/authService";
-import { generateAccessToken, generateRefreshToken, verifyRefreshToken } from "../services/tokenService";
-import { RefreshTokenRepository } from "../repositories/refreshTokenRepository";
-
-const refreshTokenRepo = new RefreshTokenRepository();
+import { registerUser, loginUser, logoutUser } from "../services/authService";
+import { rotateTokens} from "../services/tokenService"
 
 export async function registerController(req: FastifyRequest, reply: FastifyReply) {
    const { username, password } = req.body as { username: string; password: string };
@@ -20,52 +17,63 @@ export async function loginController(req: FastifyRequest, reply: FastifyReply) 
     const { username, password } = req.body as { username: string; password: string };
 
     try {
-        const result = await loginUser(username, password);
-        return reply.send(result);
+        const {token, refreshToken} = await loginUser(username, password);
+
+        reply.setCookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/auth/refresh",
+            maxAge: 7 * 24 * 60 * 60,
+        });
+
+        return reply.send({ accessToken: token});
+
     } catch (err: any) {
         return reply.code(401).send({ error: err.message });
     }
 }
 
 export async function refreshController(req: FastifyRequest, reply: FastifyReply) {
-    const { refreshToken } = req.body as { refreshToken: string };
+    const refreshToken = req.cookies.refresh;
 
     if (!refreshToken) {
         return reply.code(400).send({ error: "Refresh token is required" });
     }
 
     try {
-        const decoded = verifyRefreshToken(refreshToken);
+        const { newAccessToken, newRefreshToken } = rotateTokens(refreshToken);
 
-        const existing = refreshTokenRepo.findByToken(refreshToken);
-        if (!existing) {
-            return reply.code(401).send({ error: "Invalid refresh token" });
-        }
-
-        const newAccessToken = generateAccessToken({ username: decoded.username, id: decoded.id });
-        const newRefreshToken = generateRefreshToken({ username: decoded.username, id: decoded.id });
-
-        refreshTokenRepo.delete(refreshToken);
-        refreshTokenRepo.add(decoded.id, newRefreshToken);
+        reply.setCookie("refreshToken", newRefreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/auth/refresh",
+            maxAge: 7 * 24 * 60 * 60
+        });
 
         return reply.send({
-            accessToken: newAccessToken,
-            refreshToken: newRefreshToken,
+            accessToken: newAccessToken
         });
+
     } catch (err: any) {
         return reply.code(401).send({ error: err.message });
     }
 }
 
 export async function logoutController(req: FastifyRequest, reply: FastifyReply) {
-    const { refreshToken } = req.body as { refreshToken?: string };
+    const refreshToken = req.cookies.refreshToken;
 
     if (!refreshToken) {
         return reply.code(400).send({ error: "Refresh token is required" });
     }
 
     try {
-        refreshTokenRepo.delete(refreshToken);
+        logoutUser(refreshToken);
+
+        reply.clearCookie("refreshToken", {
+            path: "/auth/refresh",
+        })
 
         return reply.send({ success: true, message: "Logged out successfully" });
     } catch (err) {
