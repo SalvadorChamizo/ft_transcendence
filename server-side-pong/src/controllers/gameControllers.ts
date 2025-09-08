8fe8af70b7e442fd4dd92dda7b4ff03fdebed18a
jeandrad1 <jesusandradeperez@gmail.com>
2025-09-08 13:22:50 +0200

    feat: Added the game handlers and game server
    Now the game handlers are connected with their sockets and the game server is created for the game to be played.

--- server-side-pong/src/controllers/gameControllers.ts
+++ server-side-pong/src/controllers/gameControllers.ts
@@ -1,12 +1,38 @@
 /**
  * @file gameControllers.ts
- * @brief Import io to use socket.IO server side utils
+ * @brief Import io to use socket.IO server and sockets, the the gameService and the PaddleSide
  */
 
 import { Server , Socket } from "socket.io";
 import * as gameService from "../services/gameServices";
-
+import { PaddleSide } from "../";
 
 /**
  * @brief register the player status and the paddle movement
+ * Some logs to check the connection
+ * calls to the movement methods for the paddles
+ * actualizes the gameState with each movement
  */
+export function registerGameHandlers(io: Server, socket: Socket)
+{
+  console.log("Player connected:", socket.id);
+
+  socket.emit("paddles", gameService.getGameState());
+
+  socket.on("moveUp", (side: PaddleSide) =>
+	{
+    	gameService.moveUp(side);
+    	io.emit("paddles", gameService.getGameState());
+	});
+
+  socket.on("moveDown", (side: PaddleSide) =>
+	{
+		gameService.moveDown(side);
+		io.emit("paddles", gameService.getGameState());
+	});
+
+  socket.on("disconnect", () =>
+	{
+		console.log("Player disconnected:", socket.id);
+	});
+}
--- server-side-pong/src/index.ts
+++ server-side-pong/src/index.ts
@@ -1,21 +1,48 @@
 
 /**
  * @file index.ts
- * @brief Paddle id and the vertical position. Game state: paddles positions.
+ * @brief
  */
+
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
 
 export type PaddleSide = "left" | "right";
 
 export interface PaddleState
 {
-  y: number;
+	y: number;
 }
 
 export interface GameState
 {
-  paddles: {
-    left: PaddleState;
-    right: PaddleState;
-  };
+	paddles:
+	{
+		left: PaddleState;
+		right: PaddleState;
+	};
 }
 
+import { registerGameHandlers } from "./controllers/gameControllers";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server,
+	{
+	cors: { origin: "*" },
+	});
+
+const PORT = process.env.PORT || 3000;
+
+io.on("connection", (socket) =>
+	{
+  registerGameHandlers(io, socket);
+	});
+server.listen(PORT, () =>
+	{
+		console.log(`Pong paddle server running on port ${PORT}`);
+	});
