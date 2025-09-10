f4b2f3dd08f22b9a381aa6bc9e441bc600be4c5d
pablooglez <pablogon@student.42malaga.com>
2025-09-10 16:46:56 +0200

    docs: update chatRoutes comments to English

--- chat-service/src/routes/chatRoutes.ts
+++ chat-service/src/routes/chatRoutes.ts
@@ -2,18 +2,18 @@ import { FastifyInstance } from "fastify";
 import * as chatController from "../controllers/chatController";
 
 export async function chatRoutes(fastify: FastifyInstance) {
-    // POST /conversations/:userId/messages - Enviar mensaje
+    // POST /conversations/:userId/messages - Send message
     fastify.post('/conversations/:userId/messages', chatController.sendMessageController);
     
-    // GET /conversations - Obtener todas las conversaciones del usuario
+    // GET /conversations - Get all conversations from the user
     fastify.get('/conversations', chatController.getConversationsController);
     
-    // GET /conversations/:userId/messages - Obtener mensajes de una conversación
+    // GET /conversations/:userId/messages - Retrieve messages from a conversation
     fastify.get('/conversations/:userId/messages', chatController.getMessagesController);
     
-    // POST /users/:userId/block - Bloquear usuario
+    // POST /users/:userId/block - Block user
     fastify.post('/users/:userId/block', chatController.blockUserController);
     
-    // DELETE /users/:userId/block - Desbloquear usuario
+    // DELETE /users/:userId/block - Unlock user
     fastify.delete('/users/:userId/block', chatController.unblockUserController);
 }
