ca2c842341a37e3e6bbe4e052cc06137c3a36582
saroca-f <saroca1993@gmail.com>
2025-10-18 14:35:49 +0200

    working in avatar management

--- user-managment/src/routes/usersRoutes.ts
+++ user-managment/src/routes/usersRoutes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from "fastify";
-import { registerController , register42Controller , usernameChanger , emailChanger , userGetterByEmail, getCurrentUserController,
-    userGetterByUsername, userGetterById, passwordControl } from "../controllers/usersController";
+import { registerController , register42Controller , usernameChanger , emailChanger , userGetterByEmail,
+    getCurrentUserController, userGetterByUsername, userGetterById, avatarGetterController, passwordControl } from "../controllers/usersController";
 import { getResultsController, addVictoryController, addDefeatController } from "../controllers/matchResultController";
 import { getAllUsersController } from "../controllers/friendsController";
 
 export default async (fastify: FastifyInstance) => {
     fastify.post("/register", registerController);
     fastify.post("/register42", register42Controller);
+    fastify.get("/me", getCurrentUserController);
     fastify.post("/changeUsername", usernameChanger);
     fastify.post("/changeEmail", emailChanger);
     fastify.post("/checkPassword", passwordControl);
@@ -14,8 +15,8 @@ export default async (fastify: FastifyInstance) => {
     fastify.post("/getUserByName", userGetterByUsername);
     fastify.post("/getUserById", userGetterById);
     fastify.post("/getUserByEmail", userGetterByEmail);
+    fastify.get("/getAvatar", avatarGetterController);
     fastify.get("/getResults", getResultsController);
     fastify.post("/addVictory", addVictoryController);
     fastify.post("/addDefeat", addDefeatController);
-    fastify.get("/me", getCurrentUserController);
 };
